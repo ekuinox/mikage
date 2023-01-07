1cc11003bc69919de4f82bccca2ca1f8c119f677
ekuinox <depkey@me.com>
2023-01-08 03:10:44 +0900

    wipwipwipwip

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,5 +1,9 @@
 mod mutation;
 mod query;
+mod spotify;
+mod twitter;
 
 pub use mutation::Mutation;
 pub use query::Query;
+pub use spotify::SpotifyOAuth2Client;
+pub use twitter::TwitterOAuth2Client;
