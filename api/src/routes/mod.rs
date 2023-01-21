880df13678625cc97eaa712d61af1c4cadacd147
ekuinox <depkey@me.com>
2023-01-22 00:51:25 +0900

    wipwipwipwiiiiippp

--- api/src/routes/mod.rs
+++ api/src/routes/mod.rs
@@ -5,7 +5,11 @@ use axum::{
     routing::get,
     Router,
 };
-use axum_sessions::{async_session::SessionStore, SessionLayer};
+use axum_sessions::{
+    async_session::SessionStore,
+    extractors::{ReadableSession, WritableSession},
+    SessionLayer,
+};
 use core::{services::UserService, AppState};
 use reqwest::{header::LOCATION, StatusCode};
 use serde::Deserialize;
@@ -16,8 +20,20 @@ pub struct CallbackQueryParam {
     pub state: String,
 }
 
-async fn login(State(state): State<AppState>) -> impl IntoResponse {
-    // TODO: 既にログインしているか
+async fn index(session: ReadableSession) -> impl IntoResponse {
+    if let Some(user_id) = session.get::<i32>("user_id") {
+        return format!("Logged in as {user_id}");
+    }
+    format!("Not logged in")
+}
+
+async fn login(State(state): State<AppState>, session: ReadableSession) -> impl IntoResponse {
+    if let Some(user_id) = session.get::<i32>("user_id") {
+        println!("already login as {user_id}");
+        let mut header = HeaderMap::new();
+        header.append(LOCATION, "/".parse().unwrap());
+        return (StatusCode::TEMPORARY_REDIRECT, header);
+    }
     let url = match UserService::new(state).create_spotify_redirect_url() {
         Ok(u) => u,
         Err(e) => {
@@ -33,8 +49,9 @@ async fn login(State(state): State<AppState>) -> impl IntoResponse {
 async fn callback(
     Query(query): Query<CallbackQueryParam>,
     State(state): State<AppState>,
+    mut session: WritableSession,
 ) -> impl IntoResponse {
-    let (spotify, user) = match UserService::new(state)
+    let (user, _spotify) = match UserService::new(state)
         .exchange_spotify_code(query.code, query.state)
         .await
     {
@@ -46,14 +63,22 @@ async fn callback(
     };
     let mut header = HeaderMap::new();
     header.append(LOCATION, "/".parse().unwrap());
-    dbg!(spotify, user);
+
+    if let Err(e) = session.insert("user_id", user.id) {
+        eprintln!("{e}");
+        return (StatusCode::INTERNAL_SERVER_ERROR, HeaderMap::new());
+    }
+    println!("login ok {}", user.id);
+
+    // ここでリダイレクトするからsessionにinsertしても飛んじゃうっぽい（どうしたらいい...
     (StatusCode::TEMPORARY_REDIRECT, header)
 }
 
 pub fn router(state: AppState, session_layer: SessionLayer<impl SessionStore>) -> Router {
     Router::new()
+        .route("/", get(index))
         .route("/login", get(login))
         .route("/callback", get(callback))
-        .with_state(state)
         .layer(session_layer)
+        .with_state(state)
 }
